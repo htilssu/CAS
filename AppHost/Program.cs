741fbe5737f488fe0358b21f6bcd2e87e923bc96
htilssu <tolashuu@gmail.com>
2025-04-11 21:16:44 +0700

    add Entity Framework Core packages and update project references

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,3 +1,5 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgresBuilder = builder.AddPostgres("postgres")
@@ -11,5 +13,9 @@ var identityDb = postgresBuilder.AddDatabase("identitydb");
 var redis = builder.AddRedis("redis");
 var rabbitMq = builder.AddRabbitMQ("eventbus")
     .WithLifetime(ContainerLifetime.Persistent);
+
+var catalog = builder.AddProject<Catalog_API>("catalog")
+    .WithReference(redis)
+    .WithReference(catalogDb);
 
 builder.Build().Run();
---
+++ Catalog.API/Extension/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Catalog.API.Domain.Interfaces;
+using Catalog.API.Infrastructure.Data;
+using Catalog.API.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Extension;
+
+public static class Extensions
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+       builder.AddDbContext();
+    }
+
+    private static void AddDbContext(this IHostApplicationBuilder builder)
+    {
+        builder.AddNpgsqlDbContext<CatalogDbContext>("catalogdb", configureDbContextOptions: optionsBuilder =>
+        {
+            
+        });
+    }
+}
